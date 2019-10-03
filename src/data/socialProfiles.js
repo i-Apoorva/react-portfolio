5dd0c25ce23027af938a957db2c110eefc279d7b
i-Apoorva <apoorva.ambhoj95@gmail.com>
2019-10-04 05:21:42 +0530

    using parcel-bundler

--- src/data/socialProfiles.js
+++ src/data/socialProfiles.js
@@ -1,30 +1,30 @@
-import email from '../assets/email_icon.png';
-import twitter from '../assets/twitter_icon.png';
-import github from '../assets/github_icon.png';
-import linkedin from '../assets/linkedin_icon.png';
+import emailIcon from '../assets/email_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
 
-const SOCIAL =[{
+const SOCIAL_PROFILES =[{
     id:1,
     link:'mailto:apoorva.ambhoj95@gmail.com',
-    image: email
+    image: emailIcon
       },
 {
     id:2,
     link:'https://twitter.com/tweet2Apoo',
-    image: twitter
+    image: twitterIcon
       },
 {
     id:3,
     link:'https://github.com/i-Apoorva',
-    image: github
+    image: githubIcon
       },
 {
     id:4,
     link:'https://www.linkedin.com/in/apoorvaambhoj/',
-    image: linkedin
+    image: linkedinIcon
       }
 
 
 ];
 
-export default SOCIAL;
+export default SOCIAL_PROFILES;
